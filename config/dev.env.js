209d696d3c12f3de000f15f79085257776ecb53e
Firmiana <935170397@qq.com>
2018-10-18 10:34:17 +0800

    Signed-off-by: Firmiana <935170397@qq.com>

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,6 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  // BASE_API: '"http://127.0.0.1:5289"'
-  BASE_API: '"https://api-sit"',
+  BASE_API: '"http://127.0.0.1:5500"',
   DEBUGGER: true
-  BASE_API: '"http://127.0.0.1:5500"'
 }
